4922e18e0bfa8ce12a43046c5b49662c5a22a4b0
AuronIsmajli2001 <auronismajli2001@gmail.com>
2025-03-16 21:44:16 +0100

    Setup

---
+++ backend/Domain/Enums/Role.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Enums
+{
+    public enum Role
+    {
+        [Display(Name = "SuperAdmin")]
+        SuperAdmin,
+        [Display(Name = "Staff")]
+        Staff,
+        [Display(Name = "User")]
+        User
+    }
+}
---
+++ backend/Domain/Users/User.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Users
+{
+    public class User
+    {
+        [Key]
+        public String id {  get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public String Email { get; set; }
+        public String Password { get; set; }
+        public Role Role { get; set; }
+        public DateTime Created_at { get; set; }
+        public bool Active { get; set; }
+    }
+}
