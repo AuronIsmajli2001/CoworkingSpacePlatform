72e40448c688d04839cd4985eaa4488b09796de4
vleras <vleras512@gmail.com>
2025-05-27 23:26:07 +0300

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -16,6 +16,7 @@ import Auth from "./pages/Auth";
 import EditProfile from "./pages/EditProfile";
 import MyMembership from "./pages/UserProfile/MyMembership";
 import Equipment from "./dashboard/pages/Equipment";
+import { AuthProvider } from "./context/AuthContext";
 
 function App() {
   return (
@@ -34,13 +35,14 @@ function App() {
           <Route path="/profile" element={<EditProfile />} />
           <Route path="/userprofile/mymembership" element={<MyMembership />} />
 
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/users" element={<Users />} />
-        <Route path="/reservations" element={<Reservations />} />
-        <Route path="/spaces" element={<Spaces />} />
-        <Route path="/equipment" element={<Equipment />} />
-      </Routes>
-    </Router>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/reservations" element={<Reservations />} />
+          <Route path="/spaces" element={<Spaces />} />
+          <Route path="/equipment" element={<Equipment />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
 
