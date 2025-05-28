1cea7f8ad7cac3df6da0e1aef457caf59c92d00d
vleras <vleras512@gmail.com>
2025-05-28 22:42:52 +0300

    Update Program.cs

--- backend/Api/Program.cs
+++ backend/Api/Program.cs
@@ -34,7 +34,7 @@ public class Program
             options.AddPolicy("AllowFrontend",
                 policy =>
                 {
-                    policy.WithOrigins("http://localhost:5173") // frontend Vite port
+                    policy.WithOrigins("http://localhost:5173", "http://localhost:5174") // frontend Vite port
                           .AllowAnyHeader()
                           .AllowAnyMethod()
                           .AllowCredentials();
